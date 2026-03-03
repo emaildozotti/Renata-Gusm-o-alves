1f8b1a9707804dd86391924983f0982ac6b6cece
emaildozotti <emaildozotti@gmail.com>
2026-03-03 15:35:10 -0300

    fix: update hero and bio images to final versions

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,7 @@ export default function App() {
 
                 <div className="relative rounded-[2rem] overflow-hidden shadow-2xl transition-transform duration-700 group-hover:scale-[1.02]">
                   <img
-                    src="https://i.imgur.com/W5IG5ad.jpeg"
+                    src="https://i.imgur.com/S8cwIfl.jpeg"
                     alt="Renata Gusmão Alves"
                     className="w-full h-full object-cover aspect-[4/5] scale-105"
                   />
